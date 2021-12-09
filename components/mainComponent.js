c4ff77eb492f7ee180d861cf47d7d272b267e460
David Ta <david.ta555@gmail.com>
2021-12-08 22:30:29 -0500

    Component Setup p2

--- components/mainComponent.js
+++ components/mainComponent.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import Login from "./loginComponent"
-import { USERS } from '../shared/users';
+import React, { Component } from "react";
+import Directory from "./directoryComponent";
+import Login from "./loginComponent";
+import { USERS } from "../shared/users";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: USERS
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: USERS,
+    };
+  }
 
-    render() {
-        return <Directory users={this.state.users} />;
-    }
+  render() {
+    return <Directory users={this.state.users} />;
+  }
 }
 
 export default Main;
